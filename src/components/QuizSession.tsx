89c866083d5c20f8bade01af4bdf0e8e41cb267a
Holger Dell <holgerdell@users.noreply.github.com>
2023-03-25 12:01:42 +0100

    refactor: Motivational messages.

--- src/components/QuizSession.tsx
+++ src/components/QuizSession.tsx
@@ -12,6 +12,91 @@ import { genSeed } from "../utils/genSeed"
 import { Button } from "./Button"
 import { CenterScreen } from "./CenterScreen"
 
+const great = {
+  en: [
+    "Perfect!",
+    "Outstanding work!",
+    "Fantastic job!",
+    "You're a quiz whiz!",
+    "Excellent performance!",
+    "Impressive results!",
+    "Great work!",
+    "Amazing job!",
+    "Incredible performance!",
+    "Brilliant work!",
+    "Superb job!",
+  ],
+  de: [
+    "Perfekt!",
+    "Hervorragende Arbeit!",
+    "Fantastische Arbeit!",
+    "Du bist ein Quiz-Meister!",
+    "Ausgezeichnete Leistung!",
+    "Beeindruckende Ergebnisse!",
+    "Großartige Arbeit!",
+    "Erstaunliche Arbeit!",
+    "Unglaubliche Leistung!",
+    "Brillante Arbeit!",
+    "Hervorragende Arbeit!",
+  ],
+}
+const good = {
+  en: [
+    "Nice job, keep it up!",
+    "You're on the right track!",
+    "Solid effort, keep practicing!",
+    "You're improving with each try!",
+    "Well done, but there's always room for improvement!",
+    "Good job!",
+    "Great effort!",
+    "Well played!",
+    "Impressive improvement!",
+    "You're getting there!",
+  ],
+  de: [
+    "Gute Arbeit, weiter so!",
+    "Du bist auf dem richtigen Weg!",
+    "Solide Anstrengung, weiter üben!",
+    "Du verbesserst dich mit jedem Versuch!",
+    "Gut gemacht, aber es gibt immer Raum für Verbesserungen!",
+    "Gute Arbeit!",
+    "Große Anstrengung!",
+    "Gut gespielt!",
+    "Beeindruckende Verbesserung!",
+    "Du kommst näher!",
+  ],
+}
+const meh = {
+  en: [
+    "You'll do better next time!",
+    "Not bad, keep working at it!",
+    "You're making progress, keep going!",
+    "Keep practicing, you'll get there!",
+    "Don't give up, you're improving!",
+    "A little more effort and you'll see better results!",
+    "You must try again to succeed!",
+    "Keep it up!",
+    "Stay focused!",
+    "Keep pushing!",
+    "You're improving!",
+    "You're getting better!",
+  ],
+  de: [
+    "Beim nächsten Mal wirst du es besser machen!",
+    "Nicht schlecht, weiter so!",
+    "Du machst Fortschritte, bleib dran!",
+    "Übe weiter, du wirst es schaffen!",
+    "Gib nicht auf, du verbesserst dich!",
+    "Ein wenig mehr Anstrengung und du wirst bessere Ergebnisse sehen!",
+    "Du musst es erneut versuchen, um erfolgreich zu sein!",
+    "Weiter so!",
+    "Bleib fokussiert!",
+    "Bleib dran!",
+    "Du verbesserst dich!",
+    "Du wirst besser!",
+  ],
+}
+
 /**
  * Component that renders a quiz session, consisting of a sequence of targetNum
  * questions. In practice mode, the questions are chosen among the unlocked
@@ -97,97 +182,17 @@ export function QuizSession({ mode }: { mode: "practice" | "examine" }) {
       />
     )
   } else {
-    const greatEN = [
-      "Perfect!",
-      "Outstanding work!",
-      "Fantastic job!",
-      "You're a quiz whiz!",
-      "Excellent performance!",
-      "Impressive results!",
-      "Great work!",
-      "Amazing job!",
-      "Incredible performance!",
-      "Brilliant work!",
-      "Superb job!",
-    ]
-    const goodEN = [
-      "Nice job, keep it up!",
-      "You're on the right track!",
-      "Solid effort, keep practicing!",
-      "You're improving with each try!",
-      "Well done, but there's always room for improvement!",
-      "Good job!",
-      "Great effort!",
-      "Well played!",
-      "Impressive improvement!",
-      "You're getting there!",
-    ]
-    const mehEN = [
-      "You'll do better next time!",
-      "Not bad, keep working at it!",
-      "You're making progress, keep going!",
-      "Keep practicing, you'll get there!",
-      "Don't give up, you're improving!",
-      "A little more effort and you'll see better results!",
-      "You must try again to succeed!",
-      "Keep it up!",
-      "Stay focused!",
-      "Keep pushing!",
-      "You're improving!",
-      "You're getting better!",
-    ]
-    const greatDE = [
-      "Perfekt!",
-      "Hervorragende Arbeit!",
-      "Fantastische Arbeit!",
-      "Du bist ein Quiz-Meister!",
-      "Ausgezeichnete Leistung!",
-      "Beeindruckende Ergebnisse!",
-      "Großartige Arbeit!",
-      "Erstaunliche Arbeit!",
-      "Unglaubliche Leistung!",
-      "Brillante Arbeit!",
-      "Hervorragende Arbeit!",
-    ]
-    const goodDE = [
-      "Gute Arbeit, weiter so!",
-      "Du bist auf dem richtigen Weg!",
-      "Solide Anstrengung, weiter üben!",
-      "Du verbesserst dich mit jedem Versuch!",
-      "Gut gemacht, aber es gibt immer Raum für Verbesserungen!",
-      "Gute Arbeit!",
-      "Große Anstrengung!",
-      "Gut gespielt!",
-      "Beeindruckende Verbesserung!",
-      "Du kommst näher!",
-    ]
-    const mehDE = [
-      "Beim nächsten Mal wirst du es besser machen!",
-      "Nicht schlecht, weiter so!",
-      "Du machst Fortschritte, bleib dran!",
-      "Übe weiter, du wirst es schaffen!",
-      "Gib nicht auf, du verbesserst dich!",
-      "Ein wenig mehr Anstrengung und du wirst bessere Ergebnisse sehen!",
-      "Du musst es erneut versuchen, um erfolgreich zu sein!",
-      "Weiter so!",
-      "Bleib fokussiert!",
-      "Bleib dran!",
-      "Du verbesserst dich!",
-      "Du wirst besser!",
-    ]
-    const great = i18n.language === "de" ? greatDE : greatEN
-    const good = i18n.language === "de" ? goodDE : goodEN
-    const meh = i18n.language === "de" ? mehDE : mehEN
-    const msg =
+    const msgList =
       numIncorrect == 0
-        ? random.choice(great)
+        ? great
         : numCorrect / (numCorrect + numIncorrect) >= 0.75
-        ? random.choice(good)
-        : random.choice(meh)
+        ? good
+        : meh
+    const msg = random.choice(msgList[i18n.language === "de" ? "de" : "en"])
     return (
       <CenterScreen>
         <div className="w-full rounded-xl bg-black/10 p-16 dark:bg-black/20">
-          <div>&quot;{msg}&quot;</div>
+          <div className="font-serif italic">{msg}</div>
           <Button to={"/"} color="green" className="mt-12 ml-auto">
             Continue
           </Button>
