84d5f6bccf9d3f6ca8566456a801da391b33b89a
Holger Dell <holgerdell@users.noreply.github.com>
2023-04-02 10:25:09 +0200

    chore: Reorganize about page

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -9,15 +9,16 @@ export function About() {
     <div className="mx-auto mb-16 max-w-lg p-3">
       <h1>{t("About")}</h1>
       <p>
-        This website is a learning tool that aims to help you practice some of
+        This application helps you practice some of
         the skills that are relevant for the exam of a typical algorithms and
         data structures course. The site is supposed to be both useful and fun!
       </p>
-      <h2 className="mt-5">Algorithmic strength meter</h2>
+      <h2 className="mt-5">Active learning</h2>
       <p>
-        The site uses a simple artificial intelligence algorithm to learn which
-        problems you tend to struggle with, and present them to you more often.
-        Over time, the algorithm adapts to your demonstrated skill strength.
+        This site is supposed to complement lectures, videos, and textbooks by
+        providing you with a set of interactive exercises that will help you to
+        practice the skills that you have seen in class. You get instant
+        feedback and can try as often as you like.
       </p>
       <h2 className="mt-5">Spaced repetition</h2>
       <p>
@@ -31,29 +32,29 @@ export function About() {
         increasing intervals between the repetitions. This way, you will be able
         to remember the skill for a much longer time.
       </p>
-      <h2 className="mt-5">Active learning</h2>
+      <h2 className="mt-5">Individually adaptive</h2>
       <p>
-        You can only learn by doing. This site is supposed to complement
-        lectures, videos, and textbooks by providing you with a set of
-        interactive exercises that will help you to practice the skills that you
-        have seen in class.
+        To implement spaced repetition, we use{" "}
+        <Link to="https://doi.org/10.18653/v1/p16-1174">
+          half-life regression
+        </Link>{" "}
+        as the model for how your memory works. We employ a simple artificial
+        intelligence algorithm to identify which problems you tend to struggle
+        with, and present them to you more often. Over time, the algorithm
+        adapts to your demonstrated skill strength.
       </p>
       <h2 className="mt-5">Development</h2>
       <p>
         This site is under heavy development, utterly incomplete, and things
         might break without notice! Our plan for this site is to provide
-        exam-level questions for all intended learning outcomes at levels
-        SOLO1/SOLO2 according to{" "}
-        <Link to="https://en.wikipedia.org/wiki/Bloom's_taxonomy">
+        exam-level questions for as many intended learning outcomes of the
+        algorithms and data structures courses. This should be possible for all
+        learning outcomes at levels &ldquo;remember/understand/apply&rdquo;
+        according to{" "}
+        <Link to="https://cft.vanderbilt.edu/guides-sub-pages/blooms-taxonomy/">
           Bloom&apos;s taxonomy
         </Link>
-        . We are also looking into making the algorithm better, and integrating
-        the site with Moodle.
-      </p>
-      <h2 className="mt-5">Authors</h2>
-      <p>
-        Created by <a href="https://holgerdell.com">Holger Dell</a> (2023) with
-        some help by GPT-4 and GitHub Copilot.
+        .
       </p>
       <h2 className="mt-5">Source Code</h2>
       <p>
@@ -78,6 +79,10 @@ export function About() {
         </Link>{" "}
         and its research division{" "}
         <Link to="https://research.duolingo.com/">duolingo research</Link>.
+      </p>{" "}
+      <h2 className="mt-5">Authors</h2>
+      <p>
+        Created by <a href="https://holgerdell.com">Holger Dell</a> (2023).
       </p>
     </div>
   )
