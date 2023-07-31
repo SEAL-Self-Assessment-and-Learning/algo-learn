aea862b75cc694a3af2a0ba775717faedfd5dcbe
nico_miarka <nico.miarka@gmail.com>
2023-07-31 04:44:02 +0200

    feat: Added master theorem exercise Closes #10

--- shared/src/question-generators/recursion/formulaUtils.ts
+++ shared/src/question-generators/recursion/formulaUtils.ts
@@ -1,4 +1,8 @@
+import { log } from "mathjs"
 import Random from "../../utils/random"
+import { ProductTerm } from "../asymptotics/asymptoticsUtils"
+import { createProductTerm } from "../asymptotics/asymptoticsUtils"
+import Fraction from "fraction.js"
 
 /**
  * Produce a string of the form " print(***)"
@@ -179,4 +183,154 @@ export function parseRecursiveFunction(input: string): {
   const c = match[5] ? parseInt(match[5], 10) : 0
 
   return { a, T, n, b, c }
+}
+/**
+ * Sample a recursive function T(n) = a T(n/b) + c, T(1) = d that the master
+ * theorem can be applied on.
+ *
+ * @param random - Random number generator
+ * @returns - Returns the parameters a, b, c , d, the Theta runtime of the
+ *   function, as well as which master theorem case is being used.
+ */
+export function sampleMasterRecursion(random: Random) {
+  const base = random.int(2, 10)
+  //get a random exponent, so that the power is below 150 for reasonable recursion functions.
+  const result = base ** random.int(1, Math.floor(log(150, base)))
+  let a = base
+  let b = result
+  if (random.int(0, 1) === 0) {
+    ;[a, b] = [b, a]
+  }
+  a = random.weightedChoice([
+    [a, 0.85],
+    [1, 0.15],
+  ])
+  let c: ProductTerm
+  const masterCase = a === 1 ? random.int(2, 3) : random.int(1, 3)
+  let solution: ProductTerm
+  const coefficient = random.int(2, 99)
+  if (masterCase === 1) {
+    const polyexponent =
+      log(a, b) <= 1
+        ? new Fraction(1, Math.round(log(b, a)) + 1)
+        : log(a, b) - 1
+    c = createProductTerm({
+      coefficient: coefficient,
+      polyexponent: polyexponent,
+    })
+    solution = createProductTerm({
+      coefficient: coefficient,
+      polyexponent: log(a, b),
+    })
+  } else if (masterCase === 2) {
+    c = createProductTerm({ coefficient: coefficient, polyexponent: log(a, b) })
+    solution = createProductTerm({
+      coefficient: coefficient,
+      polyexponent: log(a, b),
+      logexponent: 1,
+    })
+  } else {
+    const polyexponent =
+      a === 1
+        ? random.choice([random.int(1, 2), new Fraction(1, random.int(2, 3))])
+        : log(a, b) < 1
+        ? new Fraction(random.int(1, 3), Math.max(Math.round(log(b, a)) - 1, 1))
+        : log(a, b) + 1
+    c = createProductTerm({
+      coefficient: coefficient,
+      polyexponent: polyexponent,
+    })
+    solution = c
+  }
+  return {
+    b: b,
+    a: a,
+    c: c,
+    d: random.int(2, 9),
+    solution: solution,
+    masterCase: masterCase,
+  }
+}
+/**
+ * Sample a set of Theta runtimes. The runtimes are: Theta(n^log_b(a)),
+ * Theta(n^log_b(a) * log(n)), Theta(c) and Theta(c * log(n)) If c = n^logb(a)
+ * (master theorem case 2) then Theta(c) and Theta(c * log(n)) get randomized.
+ *
+ * @returns Array of objects containing the key, whether it is correct, and the
+ *   element to render
+ */
+export function sampleMasterRecursionAnswers({
+  random,
+  masterCase,
+  a,
+  b,
+  c,
+}: {
+  random: Random
+  masterCase: number
+  a: number
+  b: number
+  c: ProductTerm
+}): Array<{ key: string; correct: boolean; element: string }> {
+  const answers: Array<{
+    key: string
+    correct: boolean
+    element: string
+  }> = []
+  function runTime(a: number, b: number) {
+    return log(a, b) === 0
+      ? "1"
+      : log(a, b) === 1
+      ? "n"
+      : log(a, b) >= 1
+      ? `n^${Math.round(log(a, b))}`
+      : createProductTerm({
+          coefficient: 1,
+          polyexponent: new Fraction(1, Math.round(log(b, a))),
+        }).toLatex("n")
+  }
+  answers.push({
+    key: "1",
+    correct: masterCase === 1,
+    element: `\\Theta(${runTime(a, b)})`,
+  })
+  answers.push({
+    key: "2",
+    correct: masterCase === 2,
+    element: `\\Theta(${log(a, b) === 0 ? "" : runTime(a, b)} \\log(n))`,
+  })
+  let secondTerm = createProductTerm({
+    coefficient: new Fraction(1),
+    polyexponent: c.logarithmExponents.get(0),
+  }).toLatex("n")
+  if (masterCase === 2) {
+    secondTerm =
+      log(a, b) === 0
+        ? "n"
+        : log(a, b) === 1
+        ? "n^2"
+        : log(a, b) >= 1
+        ? createProductTerm({
+            coefficient: 1,
+            polyexponent: Math.round(log(a, b)) + random.choice([-1, 1]),
+          }).toLatex("n")
+        : createProductTerm({
+            coefficient: 1,
+            polyexponent: new Fraction(
+              random.int(1, 2),
+              Math.round(log(b, a)) + random.choice([-1, 1]),
+            ),
+          }).toLatex("n")
+  }
+  answers.push({
+    key: "3",
+    correct: masterCase === 3,
+    element: `\\Theta(${secondTerm})`,
+  })
+  answers.push({
+    key: "4",
+    correct: false,
+    element: `\\Theta(${secondTerm === "1" ? "" : secondTerm} \\log(n))`,
+  })
+  return random.shuffle(answers)
 }
---
+++ shared/src/question-generators/recursion/recurrenceMaster.ts
@@ -0,0 +1,194 @@
+import { validateParameters } from "../../api/Parameters"
+import {
+  FreeTextFeedbackFunction,
+  FreeTextFormatFunction,
+  minimalMultipleChoiceFeedback,
+  Question,
+  QuestionGenerator,
+} from "../../api/QuestionGenerator"
+import { serializeGeneratorCall } from "../../api/QuestionRouter"
+import { format } from "../../utils/format"
+import Random from "../../utils/random"
+import { tFunction, tFunctional, Translations } from "../../utils/translations"
+import {
+  sampleMasterRecursion,
+  sampleMasterRecursionAnswers,
+} from "./formulaUtils"
+import { MathNode } from "mathjs"
+import math, { getVars } from "../../utils/math"
+import {
+  mathNodeToSumProductTerm,
+  SumProductTerm,
+} from "../asymptotics/asymptoticsUtils"
+import Fraction from "fraction.js"
+const translations: Translations = {
+  en_US: {
+    code: "\\[{{0}}\\]",
+    description:
+      "Let $ {{0}}(n) $ for positive integers $n$ be defined via the following recurrence relations:\\[{{1}}\\] \\[{{2}}\\,.\\]",
+    description2: `Use the master theorem to find a closed form of $ {{0}} $ in $\\Theta$-notation.`,
+    name: "RecurrenceMaster",
+    bottomnote:
+      "Note: You can assume that $n = {{0}}$ for a $k \\in \\mathbb{N}$ with $k>0$",
+    "feedback.incomplete": "Incomplete or too complex",
+    "feedback.unknown-variable": "Unknown variable",
+    "feedback.expected": "Expected",
+    "feedback.invalid-expression": "invalid formula",
+    "feedback.wrong.case1":
+      "We are in case 1, because $f(n) = \\Oh\\left(n^log_{b}(a) - \\espilon\\right)$",
+  },
+  de_DE: {
+    code: "\\[{{0}}\\]",
+    description:
+      "Sei $ {{0}}(n) $ für positive ganze Zahlen $n$ definiert durch die folgende Rekursionsgleichung:\\[{{1}}\\] \\[{{2}}\\,.\\]",
+    description2: `Verwende das Mastertheorem, um für $ {{0}} $ eine geschlossene Form in $\\Theta$-Notation zu erhalten.`,
+    name: "RecurrenceMaster",
+    bottomnote:
+      "Hinweis: Du kannst davon ausgehen, dass $n = {{0}}$ gilt für ein $k\\in \\mathbb{N}$ mit $k>0$",
+    "feedback.incomplete": "Unvollständig oder zu komplex",
+    "feedback.unknown-variable": "Unbekannte Variable",
+    "feedback.expected": "Erwartet",
+    "feedback.invalid-expression": "ungültige Formel",
+    "feedback.wrong.case1":
+      "Wir sind in Fall 1, da $f(n) = \\Oh\\left(n^log_{b}(a) - \\espilon\\right)$",
+  },
+}
+
+export const RecurrenceMaster: QuestionGenerator = {
+  path: "recursion/master",
+  name: tFunctional(translations, "name"),
+  languages: ["en_US", "de_DE"],
+  expectedParameters: [
+    {
+      type: "string",
+      name: "variant",
+      allowedValues: ["choice", "input"],
+    },
+  ],
+  generate(lang, parameters, seed) {
+    const permalink = serializeGeneratorCall({
+      generator: RecurrenceMaster,
+      lang,
+      parameters,
+      seed,
+    })
+    const random = new Random(seed)
+    const { t } = tFunction(translations, lang)
+
+    if (!validateParameters(parameters, RecurrenceMaster.expectedParameters)) {
+      throw new Error(
+        `Unknown variant ${
+          parameters.variant
+        }. Valid variants are: ${RecurrenceMaster.expectedParameters.join(
+          ", ",
+        )}`,
+      )
+    }
+
+    const variant = parameters.variant as "choice" | "input"
+    const { b, a, d, c, solution, masterCase } = sampleMasterRecursion(random)
+
+    const T = random.choice("TABCDEFGHS".split(""))
+    const answers = sampleMasterRecursionAnswers({
+      random,
+      masterCase,
+      a,
+      b,
+      c,
+    })
+    const text = `
+    ${format(t("description"), [
+      T,
+      `${T}(n) = ${a === 1 ? "" : a} ${T}(n/${b}) + ${c.toLatex("n")}`,
+      `${T}(1) = ${d}`,
+    ])}
+    
+    ${format(t("description2"), [`${T}(n)`])}`
+    let question: Question
+    if (variant === "choice") {
+      question = {
+        type: "MultipleChoiceQuestion",
+        name: t("name"),
+        path: permalink,
+        text: text,
+        answers: answers.map(
+          ({ element }) => `${format(t("code"), [element])}`,
+        ),
+        feedback: minimalMultipleChoiceFeedback({
+          correctAnswerIndex: answers
+            .map((x, i) => ({ ...x, i }))
+            .filter((x) => x.correct)
+            .map((x) => x.i),
+        }),
+      }
+    } else {
+      const prompt = `$\\Theta$ `
+      const bottomText = t("bottomnote", [`${b}^k`])
+      const checkFormat: FreeTextFormatFunction = ({ text }) => {
+        if (text.trim() === "") return { valid: false }
+        let mathNode: MathNode
+        try {
+          mathNode = math.parse(text)
+          const unknownVars = getVars(mathNode).filter((v) => v !== "n")
+          const unknownVar: string | null =
+            unknownVars.length > 0 ? unknownVars[0] : null
+          if (unknownVar) {
+            return {
+              valid: false,
+              message: `${t("feedback.unknown-variable")}: $${unknownVar}$. ${t(
+                "feedback.expected",
+              )}: $${"n"}$.`,
+            }
+          }
+          try {
+            mathNodeToSumProductTerm(math.parse(text))
+            return {
+              valid: true,
+              message:
+                "$" +
+                mathNode.toTex({
+                  parenthesis: "auto",
+                  implicit: "show",
+                }) +
+                "$",
+            }
+          } catch (e) {
+            return {
+              valid: false,
+              message: t("feedback.incomplete"),
+            }
+          }
+        } catch (e) {
+          return {
+            valid: false,
+            message: t("feedback.invalid-expression"),
+          }
+        }
+      }
+
+      const feedback: FreeTextFeedbackFunction = ({ text }) => {
+        solution.coefficient = new Fraction(1)
+        const correctAnswer = "$\\Theta(" + solution.toLatex("n") + ")$"
+        const sumProductTerm: SumProductTerm = mathNodeToSumProductTerm(
+          math.parse(text),
+        )
+
+        return {
+          correct: solution.Theta(sumProductTerm.dominantTerm()),
+          correctAnswer,
+        }
+      }
+      question = {
+        type: "FreeTextQuestion",
+        path: permalink,
+        name: t("name"),
+        text,
+        prompt,
+        bottomText,
+        feedback,
+        checkFormat,
+      }
+    }
+    return { question }
+  },
+}
