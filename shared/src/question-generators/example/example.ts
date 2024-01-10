90bd611fc83fae523b6f2f0e7b97df7edecd62a2
Alex Schickedanz <alex@ae.cs.uni-frankfurt.de>
2024-01-10 14:48:57 +0100

    Polish example question generator

--- shared/src/question-generators/example/example.ts
+++ shared/src/question-generators/example/example.ts
@@ -5,24 +5,44 @@ import {
 } from "../../api/QuestionGenerator"
 import { serializeGeneratorCall } from "../../api/QuestionRouter"
 import Random from "../../utils/random"
-import { tFunction, tFunctional, Translations } from "../../utils/translations"
+import { t, tFunctional, Translations } from "../../utils/translations"
 
+/**
+ * All text displayed text goes into the translations object.
+ * Include at least english and german.
+ */
 const translations: Translations = {
   en: {
     name: "Compute a sum",
     description: "Compute the sum of two integers",
-    text: "Let {{0}} and {{1}} be two natural numbers. What is the **sum** {{2}}?",
-    seedDescription: "Seed for the random number generator",
+    text: "Let ${{0}}$ and ${{1}}$ be two natural numbers. What is the **sum** ${{0}}+{{1}}$?",
   },
   de: {
     name: "Summe berechnen",
     description: "Berechne die Summe zweier Zahlen",
-    text: "Seien {{0}} und {{1}} zwei natürliche Zahlen. Was ist die **Summe** {{2}}?",
-    seedDescription: "Seed für den Zufallsgenerator",
+    text: "Seien ${{0}}$ und ${{1}}$ zwei natürliche Zahlen. Was ist die **Summe** ${{0}}+{{1}}$?",
   },
 }
 
-/** This question generator generates a simple multiple choice question. */
+function generateWrongAnswers(
+  random: Random,
+  correctAnswer: number,
+): Array<string> {
+  let wrongAnswers = [
+    `$${correctAnswer + 3}$`,
+    `$${correctAnswer + 2}$`,
+    `$${correctAnswer + 1}$`,
+    `$${correctAnswer - 1}$`,
+    `$${correctAnswer - 2}$`,
+    `$${correctAnswer - 3}$`,
+  ]
+
+  return random.subset(wrongAnswers, 3)
+}
+
+/**
+ * This question generator generates a simple multiple choice question.
+ */
 export const ExampleQuestion: QuestionGenerator = {
   name: tFunctional(translations, "name"),
   description: tFunctional(translations, "description"),
@@ -34,37 +54,32 @@ export const ExampleQuestion: QuestionGenerator = {
   expectedParameters: [],
 
   /**
-   * Generates a new SimpleMCTest question.
+   * Generates a new MultipleChoiceQuestion question.
    *
-   * @param props
-   * @param props.seed The seed for the random number generator
+   * @generatorPath The path the generator is located on the page. Defined in settings/questionSelection.ts
    * @param lang The language of the question
-   * @returns A new SimpleMCTest question
+   * @param parameters The parameters for the question. In this case none are used.
+   * @param seed The seed for the random number generator
+   * @returns A new MultipleChoiceQuestion question
    */
   generate: (generatorPath, lang = "en", parameters, seed) => {
+    // initialize the RNG so the question can be generated again
     const random = new Random(seed)
 
-    const a = random.int(2, 10)
-    const b = random.int(2, 10)
+    // generate the question values
+    const a = random.int(5, 15)
+    const b = random.int(5, 15)
     const correctAnswer = a + b
-    const answers = [
-      `$${correctAnswer}$`,
-      `$${correctAnswer + 1}$`,
-      `$${correctAnswer - 1}$`,
-    ]
 
-    let c
-    do {
-      c = random.int(4, 20)
-    }
-    while (answers.includes(`$${c}$`))
-    answers.push(`$${c}$`)
+    // get a set of wrong answers
+    const answers = generateWrongAnswers(random, correctAnswer)
 
+    // add the correct answer, shuffle and find the index of the correct answer
+    answers.push(`$${correctAnswer}$`)
     random.shuffle(answers)
     const correctAnswerIndex = answers.indexOf(`$${correctAnswer}$`)
 
-    const { t } = tFunction(translations, lang)
-
+    // generate the question object
     const question: MultipleChoiceQuestion = {
       type: "MultipleChoiceQuestion",
       name: ExampleQuestion.name(lang),
@@ -75,17 +90,20 @@ export const ExampleQuestion: QuestionGenerator = {
         seed,
         generatorPath,
       }),
-      text: t("text", [`$${a}$`, `$${b}$`, `$${a}+${b}$`]),
+      text: t(translations, lang, "text", [`${a}`, `${b}`]),
       answers,
       feedback: minimalMultipleChoiceFeedback({ correctAnswerIndex }),
     }
 
     return {
       question,
-      a, // only added for possible unit tests
-      b, // only added for possible unit tests
-      correctAnswer, // only added for possible unit tests
-      correctAnswerIndex, // only added for possible unit tests
+      // allows for unit tests
+      testing: {
+        a,
+        b,
+        correctAnswer,
+        correctAnswerIndex,
+      },
     }
   },
 }
