95b29e380b0e478f914a4ffe2605c354a0d0ce9b
fabianstw <fstiewe@aol.com>
2024-10-02 16:13:06 +0200

    Changed task s.t. the user understands more easily the heap is represented as an array

--- shared/src/question-generators/heap/generatorOperations.ts
+++ shared/src/question-generators/heap/generatorOperations.ts
@@ -17,26 +17,26 @@ const translationHeapOperations: Translations = {
     name: "Heap-Operations",
     description: "Determine the state of a Heap after various operations",
     taskInsert:
-      "Consider a **{{0}}-Heap**. Initally the Heap is empty. Insert the elements in the given order: \\[{{1}}\\] Enter the final state of the Heap.",
+      "Consider a **{{0}}-Heap**. Initially the Heap is empty. Insert the elements in the given order: \\[{{1}}\\] Enter the final state of the Heap as an array.",
     taskExtract:
-      "Consider the following **{{0}}-Heap**: {{1}} You call `Extract{{2}}` **{{3}}** times. Enter the final state of the Heap.",
+      "Consider a **{{0}}-Heap** that is given by the following array: {{1}} You call `Extract{{2}}` **{{3}}** times. Enter the final state of the Heap as an array.",
     taskBuild:
-      "Consider the following **Array**: {{0}} Enter the state of the Heap after **Build-{{1}}-Heap** on the above array.",
+      "(Not included at the moment) Consider the following **Array**: {{0}} Enter the state of the Heap after **Build-{{1}}-Heap** on the above array.",
     taskCombine:
-      "Consider the following sequence of operations, where a **number** represents **inserting** this number into the **{{1}}-Heap** and $*$ represents an `Extract{{1}}` operation. \\[{{0}}\\] Enter the final state of the Heap. Initially the Heap is empty.",
+      "Consider the following sequence of operations, where a **number** represents **inserting** this number into the **{{1}}-Heap** and $*$ represents an `Extract{{1}}` operation. \\[{{0}}\\] Enter the final state of the Heap as an array. Initially the Heap is empty.",
     checkFormat: "Please only enter Integer seperated by commas.",
   },
   de: {
     name: "Heap-Operationen",
     description: "Bestimme den Zustand eines Heaps nach unterschiedlichen Operationen",
     taskInsert:
-      "Betrachte einen **{{0}}-Heap**. Anfänglich ist der Heap leer. Du fügst die folgenden Elemente in gegebener Reihenfolge ein: \\[{{1}}\\] Gib den finalen Zustand des Heaps an.",
+      "Betrachte einen **{{0}}-Heap**. Anfänglich ist der Heap leer. Du fügst die folgenden Elemente in gegebener Reihenfolge ein: \\[{{1}}\\] Gib den finalen Zustand des Heaps als Array an.",
     taskExtract:
-      "Betrachte den folgenden **{{0}}-Heap**: {{1}} Du rufst `Extract{{2}}` **{{3}}** mal auf. Gib den finalen Zustand des Heaps an.",
+      "Betrachte einen **{{0}}-Heap**, der durch das folgende Array gegeben ist: {{1}} Du rufst `Extract{{2}}` **{{3}}** mal auf. Gib den finalen Zustand des Heaps als Array an.",
     taskBuild:
-      "Betrachte das folgende Array: {{0}} Gib den Heap nach **Build-{{1}}-Heap** auf dem obigen Array an.",
+      "(Not included at the moment) Betrachte das folgende Array: {{0}} Gib den Heap nach **Build-{{1}}-Heap** auf dem obigen Array an.",
     taskCombine:
-      "Betrachte die folgende Sequenz von Operationen, wobei eine **Zahl** das **Einfügen** dieser Zahl in den **{{1}}-Heap** repräsentiert und $*$ eine `Extract{{1}}` Operation. \\[{{0}}\\] Gib den finalen Zustand des Heaps an. Anfänglich ist der Heap leer.",
+      "Betrachte die folgende Sequenz von Operationen, wobei eine **Zahl** das **Einfügen** dieser Zahl in den **{{1}}-Heap** repräsentiert und $*$ eine `Extract{{1}}` Operation. \\[{{0}}\\] Gib den finalen Zustand des Heaps als Array an. Anfänglich ist der Heap leer.",
     checkFormat: "Bitte nur ganze Zahlen durch Komma getrennt eingeben.",
   },
 }
