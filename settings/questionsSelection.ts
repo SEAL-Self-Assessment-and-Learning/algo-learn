b864400622962c3bff0f7f53d04d011ce845bfd5
welkerje <welker@em.uni-frankfurt.de>
2024-10-16 16:44:52 +0200

    removed simple modArithmetic generator from view

--- settings/questionsSelection.ts
+++ settings/questionsSelection.ts
@@ -17,7 +17,6 @@ import { RecursionFormula } from "@shared/question-generators/recursion/formula"
 import { RecurrenceMaster } from "@shared/question-generators/recursion/recurrenceMaster"
 import { stackQuestion } from "@shared/question-generators/Stack/StackGenerator.ts"
 import { Loops } from "@shared/question-generators/time/loops"
-import { ModularArithmetic } from "@shared/question-generators/math/modularArithmetic"
 import { ModTricks } from "@shared/question-generators/math/modTricks"
 
 export const DEFAULT_IMAGE = new URL("../../assets/images/skill-default.jpg", import.meta.url)
@@ -67,11 +66,6 @@ export const collection: QuestionCollection = [
     contents: [Satisfiability, NormalForms],
   },
   {
-    slug: "modular-arithmetic",
-    name: { de: "Modulare Arithmetik", en: "Modular Arithmetic" },
-    contents: [ModularArithmetic],
-  },  
-  {
     slug: "modular-arithmetic-tricks",
     name: { de: "Modulare Arithmetik Tricks", en: "Modular Arithmetic Tricks" },
     contents: [ModTricks],
@@ -90,6 +84,5 @@ export const oldPathToGenerator = {
   "huffmancoding/huffmanCoding": huffmanCoding,
   "stackqueue/stack": stackQuestion,
   "stackqueue/queue": queueQuestion,
-  "math/modularArithmetic": ModularArithmetic,
   "math/modTricks": ModTricks,
 }
