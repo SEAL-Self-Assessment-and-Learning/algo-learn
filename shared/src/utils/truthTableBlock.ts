9da0661aa433965bc2a156099113113b52e8b0b2
fabianstw <fstiewe@aol.com>
2024-10-19 10:40:39 +0200

    Added function to create input-fields for the truthTableComp

--- shared/src/utils/truthTableBlock.ts
+++ shared/src/utils/truthTableBlock.ts
@@ -7,6 +7,21 @@ export type TruthTableProps = {
 }
 
 /**
+ * Creates n input-field-IDs and n input-fields to provide to a truth table
+ * Those input-fields are already with correct classes aso
+ * @param numberOfFields
+ */
+export function createTruthTableInputFields(numberOfFields: number) {
+  const fieldIDs: string[] = []
+  const inputFields: string[] = []
+  for (let i = 0; i < numberOfFields; i++) {
+    fieldIDs.push("truthInput-" + i)
+    inputFields.push(`{{${fieldIDs[i]}#TTABLE###overlay}}`)
+  }
+  return { fieldIDs, inputFields }
+}
+
+/**
  * Creates a stringified object to parse via Markdown to use a TruthTable
  * Please provide function already as function.toString()
  * @param functions
