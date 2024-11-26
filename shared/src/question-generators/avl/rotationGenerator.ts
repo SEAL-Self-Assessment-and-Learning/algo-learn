e598e98e41d727cc7b537802fdbe2ccc3167d6ff
fabianstw <fstiewe@aol.com>
2024-11-26 09:57:24 +0100

    AVL tree generator about rotations.

--- shared/src/question-generators/avl/rotationGenerator.ts
+++ shared/src/question-generators/avl/rotationGenerator.ts
@@ -1,13 +1,15 @@
 import {
+  FreeTextQuestion,
   minimalMultipleChoiceFeedback,
   MultipleChoiceQuestion,
   QuestionGenerator,
 } from "@shared/api/QuestionGenerator.ts"
 import { serializeGeneratorCall } from "@shared/api/QuestionRouter.ts"
 import {
-  AVLTreeRotations,
-  avlTreeWeightedRotations,
-} from "@shared/question-generators/avl/utils/utils.ts"
+  getAVLRotationBase,
+  getReverseFeedback,
+  getRotationIndex,
+} from "@shared/question-generators/avl/utils/rotationGen.ts"
 import { generateAVLTreeInsert } from "@shared/question-generators/avl/utils/utilsInsert.ts"
 import Random from "@shared/utils/random.ts"
 import { t, tFunctional, Translations } from "@shared/utils/translations.ts"
@@ -16,8 +18,10 @@ const translations: Translations = {
   en: {
     name: "AVL-Tree Rotations",
     description: "Correctly identify AVL-Tree Rotations",
-    taskChoice:
-      "Given the following AVL-Tree: {{0}} We insert the value {{1}} into the tree. Which rotation is performed?",
+    taskStart:
+      "Given the following AVL-Tree: {{0}} We insert the value `{{1}}` into the tree. Which rotation is performed?",
+    taskReverse:
+      "Given the following AVL-Tree $A$: {{0}} We perform `A.insert(x)`. Provide a value for $x$, such that $A$ does a **{{1}}**.",
     noneR: "No rotation",
     leftR: "Left rotation",
     rightR: "Right rotation",
@@ -27,9 +31,23 @@ const translations: Translations = {
   de: {
     name: "AVL-Tree Rotationen",
     description: "Identifiziere korrekte AVL-Tree Rotationen",
+    taskStart:
+      "Gegeben ist der folgende AVL-Baum: {{0}}. Wir fügen den Wert `{{1}}` in den Baum ein. Welche Rotation wird ausgeführt?",
+    taskReverse:
+      "Gegeben ist der folgende AVL-Baum $A$: {{0}}. Wir führen `A.insert(x)` aus. Geben Sie einen Wert für $x$ an, sodass $A$ eine **{{1}}** ausführt.",
+    noneR: "Keine Rotation",
+    leftR: "Links-Drehung",
+    rightR: "Rechts-Drehung",
+    leftRightR: "Links-Rechts-Drehung",
+    rightLeftR: "Rechts-Links-Drehung",
   },
 }
 
+/**
+ * Provides an AVL Tree to the user and either ask
+ * - if insert(x) which rotation is the tree performing
+ * - if tree does should do rotation y, which value has to be inserted
+ */
 export const AVLRotationGenerator: QuestionGenerator = {
   id: "avlrot",
   name: tFunctional(translations, "name"),
@@ -57,23 +75,48 @@ export const AVLRotationGenerator: QuestionGenerator = {
 
     if (variant === "start") {
       return avlStartQuestion(random, lang, permalink)
+    } else {
+      return avlReverseQuestion(random, lang, permalink)
     }
-
-    return avlStartQuestion(random, lang, permalink)
   },
 }
 
+/**
+ * Creates the question for variant "reverse"
+ */
+function avlReverseQuestion(random: Random, lang: "de" | "en", permalink: string) {
+  const { avlTreeSize, rotationOption, rotationOptions } = getAVLRotationBase(random, translations, lang)
+  const { avlTree, askValue: possibleAnswer } = generateAVLTreeInsert({
+    random,
+    avlTreeSize,
+    rotationOption,
+  })
+
+  const graphTree = avlTree.toRootedTree().toGraph()
+  graphTree.nodeDraggable = false
+
+  const question: FreeTextQuestion = {
+    type: "FreeTextQuestion",
+    path: permalink,
+    name: AVLRotationGenerator.name(lang),
+    text: t(translations, lang, "taskReverse", [
+      graphTree.toMarkdown(),
+      rotationOptions[getRotationIndex(rotationOption)],
+    ]),
+    prompt: "$x=$",
+    feedback: getReverseFeedback(avlTree, rotationOption, possibleAnswer),
+  }
+  return {
+    question,
+  }
+}
+
+/**
+ * Creates the question for variant "start"
+ */
 function avlStartQuestion(random: Random, lang: "de" | "en", permalink: string) {
-  const avlTreeSize = random.int(6, 13)
-  const rotationOption = random.weightedChoice(avlTreeWeightedRotations)
-  const rotationOptions = [
-    t(translations, lang, "noneR"),
-    t(translations, lang, "leftR"),
-    t(translations, lang, "rightR"),
-    t(translations, lang, "leftRightR"),
-    t(translations, lang, "rightLeftR"),
-  ]
-  const correctAnswerIndex = getCorrectAnswerIndex(rotationOption)
+  const { avlTreeSize, rotationOption, rotationOptions } = getAVLRotationBase(random, translations, lang)
+  const correctAnswerIndex = getRotationIndex(rotationOption)
 
   const { avlTree, askValue } = generateAVLTreeInsert({
     random,
@@ -87,26 +130,11 @@ function avlStartQuestion(random: Random, lang: "de" | "en", permalink: string) 
     type: "MultipleChoiceQuestion",
     path: permalink,
     name: AVLRotationGenerator.name(lang),
-    text: t(translations, lang, "taskChoice", [graphTree.toMarkdown(), askValue.toString()]),
+    text: t(translations, lang, "taskStart", [graphTree.toMarkdown(), askValue.toString()]),
     answers: rotationOptions,
     feedback: minimalMultipleChoiceFeedback({ correctAnswerIndex }),
   }
   return {
     question,
-  }
-}
-
-function getCorrectAnswerIndex(rotationOption: AVLTreeRotations): number {
-  switch (rotationOption) {
-    case "none":
-      return 0
-    case "left":
-      return 1
-    case "right":
-      return 2
-    case "leftRight":
-      return 3
-    default:
-      return 4
   }
 }
---
+++ shared/src/question-generators/avl/utils/rotationGen.ts
@@ -0,0 +1,90 @@
+import { FreeTextFeedbackFunction } from "@shared/api/QuestionGenerator.ts"
+import { AVLTree } from "@shared/question-generators/avl/avlDS.ts"
+import {
+  AVLTreeRotations,
+  avlTreeWeightedRotations,
+} from "@shared/question-generators/avl/utils/utils.ts"
+import Random from "@shared/utils/random.ts"
+import { t, Translations } from "@shared/utils/translations.ts"
+
+/**
+ * Maps an AVL tree rotation option to its corresponding index.
+ *
+ * This function returns a numerical index representing the type of rotation.
+ * The indices are as follows:
+ * - "none" -> 0
+ * - "left" -> 1
+ * - "right" -> 2
+ * - "leftRight" -> 3
+ * - "rightLeft" -> 4
+ *
+ * @param {AVLTreeRotations} rotationOption - The rotation type to be mapped to an index.
+ */
+export function getRotationIndex(rotationOption: AVLTreeRotations): number {
+  switch (rotationOption) {
+    case "none":
+      return 0
+    case "left":
+      return 1
+    case "right":
+      return 2
+    case "leftRight":
+      return 3
+    default:
+      return 4
+  }
+}
+
+/**
+ * Generates a feedback function for validating user input against an AVL tree operation.
+ *
+ * @param avlTree - The AVL tree used to simulate the insertion.
+ * @param desiredRotation - The expected rotation result from the insertion.
+ * @param possibleAnswer - The correct answer that should produce the desired rotation.
+ */
+export function getReverseFeedback(
+  avlTree: AVLTree,
+  desiredRotation: AVLTreeRotations,
+  possibleAnswer: number,
+): FreeTextFeedbackFunction {
+  const feedbackWrong = {
+    correct: false,
+    correctAnswer: possibleAnswer.toString(),
+  }
+
+  return ({ text }) => {
+    const answer = text.replace(/\s/g, "")
+    if (answer === "") {
+      return feedbackWrong
+    }
+    // try to parse answer to float
+    const parsedAnswer = parseFloat(answer)
+    if (isNaN(parsedAnswer) || avlTree.insert(parsedAnswer) !== desiredRotation) {
+      return feedbackWrong
+    }
+    return {
+      correct: true,
+    }
+  }
+}
+
+/**
+ * Generates base configuration for an AVL tree rotation exercise.
+ *
+ * @returns An object containing the following properties:
+ * - `avlTreeSize` (number): A random integer between 6 and 12 representing the size of the AVL tree.
+ * - `rotationOption` (string): A rotation option.
+ * - `rotationOptions` (string[]): An array of translated rotation option names.
+ */
+export function getAVLRotationBase(random: Random, translations: Translations, lang: "en" | "de") {
+  const avlTreeSize = random.int(6, 12)
+  const rotationOption = random.weightedChoice(avlTreeWeightedRotations)
+  const rotationOptions = [
+    t(translations, lang, "noneR"),
+    t(translations, lang, "leftR"),
+    t(translations, lang, "rightR"),
+    t(translations, lang, "leftRightR"),
+    t(translations, lang, "rightLeftR"),
+  ]
+  return { avlTreeSize, rotationOption, rotationOptions }
+}
