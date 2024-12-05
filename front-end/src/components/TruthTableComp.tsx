cf66c5c3af9020370041dbde82c29e0a44642c41
fabianstw <fstiewe@aol.com>
2024-12-05 20:27:33 +0100

    Fix mini design issues from testing in branch truthTableQuestion or so

--- front-end/src/components/TruthTableComp.tsx
+++ front-end/src/components/TruthTableComp.tsx
@@ -17,54 +17,55 @@ export function TruthTableComp({ truthTableObject }: { readonly truthTableObject
       <div className={`my-5 rounded-md border-2 p-1 ${borderColor}`}>
         <table className="border-collapse">
           <thead>
-            <tr className={`rounded-t-md border-b-2 ${headerColor} ${borderColor}`}>
-              {/* Variable names */}
-              {parsedTruthTable.variables.map((x, i) => (
-                <th className={`px-3 py-2 font-black`} key={i}>
-                  <Markdown md={`$${x}$`} />
-                </th>
+          <tr className={`rounded-t-md border-b-2 ${headerColor} ${borderColor}`}>
+            {/* Variable names */}
+            {parsedTruthTable.variables.map((x, i) => (
+              <th className={`px-3 py-2 font-black`} key={i}>
+                <Markdown md={`$${x}$`} />
+              </th>
+            ))}
+
+            {/* Divider - border between variable names and functions */}
+            <th className={`border-l ${borderColor}`}></th>
+            <th className={`border-r ${borderColor}`}></th>
+
+            {/* Parsed functions */}
+            {parsedTruthTable.valuesHeader.map((func, index) => (
+              <th
+                className={`whitespace-nowrap px-6 py-2 font-black ${index === 0 ? "" : `border-l ${borderColor}`}`}
+                key={index}
+              >
+                <Markdown md={func} />
+              </th>
+            ))}
+          </tr>
+          </thead>
+          <tbody>
+          {parsedTruthTable.values.map((row, i) => (
+            <tr key={i} className={i % 2 === 1 ? cellBgColor : ""}>
+              {parsedTruthTable.variables.map((_, j) => (
+                <td className={`py-1 text-center`} key={j}>
+                  <Markdown
+                    md={`$${Math.floor(i / Math.pow(2, parsedTruthTable.variables.length - j - 1)) % 2}$`}
+                  />
+                </td>
               ))}
 
               {/* Divider - border between variable names and functions */}
               <th className={`border-l ${borderColor}`}></th>
               <th className={`border-r ${borderColor}`}></th>
 
-              {/* Parsed functions */}
-              {parsedTruthTable.valuesHeader.map((func, index) => (
-                <th
-                  className={`whitespace-nowrap px-6 py-2 font-black ${index === 0 ? "" : `border-l ${borderColor}`}`}
-                  key={index}
-                >
-                  <Markdown md={func} />
-                </th>
+              {row.map((cellValue, j) => (
+                <td className={`text-center ${j === 0 ? "" : `border-l ${borderColor}`}`} key={j}>
+                  <Markdown md={cellValue}/>
+                </td>
               ))}
             </tr>
-          </thead>
-          <tbody>
-            {parsedTruthTable.values.map((row, i) => (
-              <tr key={i} className={i % 2 === 1 ? cellBgColor : ""}>
-                {parsedTruthTable.variables.map((_, j) => (
-                  <td className={`py-1 text-center`} key={j}>
-                    <Markdown
-                      md={`$${Math.floor(i / Math.pow(2, parsedTruthTable.variables.length - j - 1)) % 2}$`}
-                    />
-                  </td>
-                ))}
-
-                {/* Divider - border between variable names and functions */}
-                <th className={`border-l ${borderColor}`}></th>
-                <th className={`border-r ${borderColor}`}></th>
-
-                {row.map((cellValue, j) => (
-                  <td key={j}>
-                    <Markdown md={cellValue} />
-                  </td>
-                ))}
-              </tr>
-            ))}
+          ))}
           </tbody>
         </table>
       </div>
     </div>
   )
 }
+
