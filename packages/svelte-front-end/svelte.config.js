e6de00437e9bb0a8bd115f912df265ded476e22e
fabianstw <fstiewe@aol.com>
2025-03-13 08:13:28 +0100

    fix: revert svelte.config.js and +layout.ts

--- packages/svelte-front-end/svelte.config.js
+++ packages/svelte-front-end/svelte.config.js
@@ -8,17 +8,15 @@ const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    // prerender: {
-    //   entries: ["*", "/en/about", "/en/legal", "/de/about", "/de/legal"],
-    // },
+    prerender: {
+      entries: ["*", "/en/about", "/en/legal", "/de/about", "/de/legal"],
+    },
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter({
       pages: "build",
-      assets: "build",
-      fallback: "index.html", // Ensures dynamic routes work
-      precompress: false,
+      fallback: "200.html"
     }),
     alias: {
       "@/*": "./src/*",
