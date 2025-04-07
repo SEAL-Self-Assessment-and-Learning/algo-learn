b582dd2c2d65b54a639c6b317b10c5a5017c53ed
fabianstw <fstiewe@aol.com>
2025-04-07 15:49:01 +0200

    back to the past

--- packages/svelte-front-end/svelte.config.js
+++ packages/svelte-front-end/svelte.config.js
@@ -9,7 +9,7 @@ const config = {
   preprocess: vitePreprocess(),
   kit: {
     prerender: {
-      entries: ["*", "/en/", "/de/", "/en/about", "/en/legal", "/de/about", "/de/legal"],
+      entries: ["*", "/en/about", "/en/legal", "/de/about", "/de/legal"],
     },
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
