d780ce188183acc640b0f49176662b8038e9cfaa
welkerje <28513807+welkerje@users.noreply.github.com>
2025-09-12 16:52:35 +0200

    refactor

---
+++ shared/src/question-generators/math/setBuilder/translations.ts
@@ -0,0 +1,27 @@
+import type { Translations } from "@shared/utils/translations"
+
+export const translations: Translations = {
+  en: {
+    name: "Set Builder Notation",
+    description: "Given a set in set-builder notation, determine all its elements explicitly.",
+    match: "Match each set given in set-builder notation with its explicit list of elements:",
+    free: "Write out all elements of the following set explicitly:",
+    checkFormat: "{a, b, c}",
+    even: "even",
+    odd: "odd",
+    prime: "prime",
+    square: "is a square",
+  },
+  de: {
+    name: "Mengenschreibweise",
+    description: "Bestimme zu einer Menge in Mengenschreibweise alle ihre Elemente explizit.",
+    match:
+      "Ordne jeder in Mengenschreibweise gegebenen Menge ihre explizite Aufzählung der Elemente zu:",
+    free: "Schreibe alle Elemente der folgenden Menge explizit auf:",
+    checkFormat: "{a, b, c}",
+    even: "gerade",
+    odd: "ungerade",
+    prime: "prim",
+    square: "ist eine Quadratzahl",
+  },
+}
