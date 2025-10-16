41cec12ae2329b5190df819f6ec07abee15d916f
Fabianstw <fstiewe@aol.com>
2025-10-16 14:03:29 +0200

    feat: enhance footer layout

--- packages/svelte-front-end/src/lib/components/Footer.svelte
+++ packages/svelte-front-end/src/lib/components/Footer.svelte
@@ -11,37 +11,22 @@
 </script>
 
 <footer class="bg-goethe mt-auto rounded-md shadow-sm">
-  <div class="mx-auto flex w-full max-w-screen-xl flex-col items-center p-4 text-center">
-    <!-- Navigation links -->
-    <ul class="mb-6 flex flex-wrap justify-center text-sm font-medium text-gray-500 dark:text-gray-400">
+  <div class="mx-auto flex w-full max-w-screen-xl flex-col items-center space-y-4 p-6 text-center">
+    <ul class="flex flex-wrap justify-center text-sm font-medium text-gray-500 dark:text-gray-400">
+      <li><a href={resolve(`/${lang}`)} class="me-4 hover:underline">{t("Home")}</a></li>
+      <li><a href={resolve(`/${lang}/about`)} class="me-4 hover:underline">{t("About.label")}</a></li>
+      <li><a href={resolve(`/${lang}/legal`)} class="me-4 hover:underline">{t("Legal.label")}</a></li>
       <li>
-        <a href={resolve(`/${lang}`)} class="me-4 hover:underline">
-          {t("Home")}
-        </a>
-      </li>
-      <li>
-        <a href={resolve(`/${lang}/about`)} class="me-4 hover:underline">
-          {t("About.label")}
-        </a>
-      </li>
-      <li>
-        <a href={resolve(`/${lang}/legal`)} class="me-4 hover:underline">
-          {t("Legal.label")}
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/SEAL-Self-Assessment-and-Learning/algo-learn"
-          class="hover:underline"
+        <a href="https://github.com/SEAL-Self-Assessment-and-Learning/algo-learn" class="hover:underline"
+          >GitHub</a
         >
-          GitHub
-        </a>
       </li>
     </ul>
-
-    <img src={SealLogo} class="h-8 brightness-0 invert filter" alt="SEAL Logo" />
-    <span class="text-2xl font-semibold whitespace-nowrap dark:text-white">
-      Self Assessment and Learning
-    </span>
+    <div class="flex flex-wrap items-center justify-center space-x-3">
+      <img src={SealLogo} class="h-8 brightness-0 invert filter" alt="SEAL Logo" />
+      <span class="text-2xl font-semibold whitespace-nowrap dark:text-white">
+        Self Assessment and Learning
+      </span>
+    </div>
   </div>
 </footer>
