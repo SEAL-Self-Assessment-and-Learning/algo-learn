8758f20b657e508d4a203361da1b13fb34e1fe0c
Alex Schickedanz <alex@ae.cs.uni-frankfurt.de>
2026-01-22 11:06:59 +0100

    Replace env variables in favor of --base parameter.

--- packages/svelte-front-end/src/lib/config.js
+++ packages/svelte-front-end/src/lib/config.js
@@ -1,8 +1,9 @@
 /** This file contains the configuration for the application. */
 
 // The base URL of the application. Useful if project is deployed to a subdirectory.
+// This can be set by `--base` in `bun run build --base /path/`
 // prettier-ignore
-export const BASENAME = import.meta.env?.VITE_PUBLIC_BASE_PATH ?? process.env?.VITE_PUBLIC_BASE_PATH ?? ""
+export const BASENAME = import.meta.env.BASE_URL
 
 // The version of the application.
 // prettier-ignore
